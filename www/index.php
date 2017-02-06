7b725f62f6a7988f43b921017d31f38b654df7e1
botirk <antipchel+github@gmail.com>
2017-02-06 17:03:07 +0300

    2nd day 2nd try

--- www/index.php
+++ www/index.php
@@ -16,10 +16,29 @@
 		</div>
 	</nav>
 	<blockquote>
-	<p>Выберете тест</p>
+		<span style="margin-right:1ex">Выберете тест или загрузите свой</span> 
+		<input type="file" name="json" style="display:inline; color:transparent;" accept=".json">
+		<script>
+			$(":file").change(function(){ if ($(":file")[0].files.length == 1){
+				var data = new FormData();	
+				data.append("json",$(":file")[0].files[0]);
+				
+				var request = new XMLHttpRequest();
+				request.onreadystatechange = function(){ if(request.readyState == 4){
+					var json = JSON.parse(request.responseText);
+					if (json.error)
+						alert(json.error);
+					else if (json.success) {
+						$("list-group").append("<a href=./test.php?test=" + json.success.slice(0,-5) + "\" class=\"list-group-item\">$file</a>");
+					}
+				}};
+				request.open("POST","./upload.php");
+				request.send(data);
+			}});
+		</script>
 		<div class="list-group">
 			<?php
-				$dirHandler = opendir("../tests_json/");
+				$dirHandler = opendir('../tests_json/');
 				while($file = readdir($dirHandler)) {
 					$file = substr($file,0,-5);
 					if ($file)
